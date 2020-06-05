0ee00017394ab59a3e75a60d1f75512ee401b88c
Aditya <adityasghadge@student.sfit.ac.in>
2020-06-06 00:46:50 +0530

    Small addition to our page content.

--- website/after/js/script.js
+++ website/after/js/script.js
@@ -1,1 +1,8 @@
-/*https://www.graphicsprings.com/filestorage/stencils/146cff6e061f3b3d77a23818faab16ab.svg*/
+$(function () {
+	$("#navbarToggle").blur(function (event) {
+		var screenWidth = window.innerWidth;
+		if (screenidth < 768) {
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
